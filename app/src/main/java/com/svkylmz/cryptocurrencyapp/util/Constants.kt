c4a3bf4e5b8ae21fbada35f2eb917b31a124ccb0
svkylmz <Ylmazsevket@gmail.com>
2021-09-13 18:17:36 +0300

    Add some utilities

---
+++ app/src/main/java/com/svkylmz/cryptocurrencyapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.svkylmz.cryptocurrencyapp.util
+
+//App explanation
+//Application that shows crypto currencies downloaded from nomics api using retrofit, jetpack compose and other some up-to-date libraries
+object Constants {
+    //https://api.nomics.com/v1/prices?key=5ed2829ec41527f78e1fe71d7a9ba3e3f8d38b1e
+    const val BASE_URL = "https://api.nomics.com/v1/"
+    const val API_KEY = "5ed2829ec41527f78e1fe71d7a9ba3e3f8d38b1e"
+    const val CALL_ATTRIBUTES = "id,name,logo_url"
+}
