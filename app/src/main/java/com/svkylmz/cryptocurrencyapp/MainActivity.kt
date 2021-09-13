4563b9297203804c73dc8404049b5c3d7248c7d9
svkylmz <Ylmazsevket@gmail.com>
2021-09-13 18:38:17 +0300

    Set basic navigation features

--- app/src/main/java/com/svkylmz/cryptocurrencyapp/MainActivity.kt
+++ app/src/main/java/com/svkylmz/cryptocurrencyapp/MainActivity.kt
@@ -3,11 +3,12 @@ package com.svkylmz.cryptocurrencyapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.remember
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
 import com.svkylmz.cryptocurrencyapp.ui.theme.CryptoCurrencyAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +16,22 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CryptoCurrencyAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                val navController = rememberNavController()
+
+                NavHost(navController = navController, startDestination = "crypto_list_screen") {
+                    composable("crypto_list_screen") {
+                        TODO("CryptoListScreen Will Be Here")
+                    }
+                    composable("crypto_detail_screen/{cryptoId}/{cryptoPrice}", arguments = listOf(
+                        navArgument("cryptoId") { type = NavType.StringType },
+                        navArgument("cryptoPrice") { type = NavType.StringType })
+                    ) {
+                        val cryptoId = remember { it.arguments?.getString("cryptoId") }
+                        val cryptoPrice = remember { it.arguments?.getString("cryptoPrice") }
+                        TODO("CryptoDetailScreen Will Be Here")
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CryptoCurrencyAppTheme {
-        Greeting("Android")
     }
 }
